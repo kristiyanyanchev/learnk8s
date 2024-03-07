d12c24639158a5edc5d80634b6a4f4998f21f215
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2024-03-07 14:41:26 +0200

    added docker file

--- hello-name.go
+++ hello-name.go
@@ -10,6 +10,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	})
-	go log.Fatal(http.ListenAndServe(":8080", nil))
+	go log.Fatal(http.ListenAndServe(":8090", nil))
 
 }
