a812d49fa582f41af2ab95f8ffcef4d798e7073c
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2024-03-09 19:52:32 +0200

    removed secrets folder no longer needed

--- secrets/decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir $HOME/secrets
-# Decrypt the file
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SECRET_PASSPHRASE" \
---output $HOME/secrets/gcp_secret.json ./secrets/encrypted_key_file_gcp.gpg
