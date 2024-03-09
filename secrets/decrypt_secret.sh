bdf92dbf553c91b6d1072c6f856cde6e56099b6e
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2024-03-09 16:28:16 +0200

    replace mkdir with cd

--- secrets/decrypt_secret.sh
+++ secrets/decrypt_secret.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+cd $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SECRET_PASSPHRASE" \
