6876cd49a3dd5bdd184eeda09d7c54cc3645a363
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2024-03-09 18:35:47 +0200

    move the gcp_secret file location

--- secrets/decrypt_secret.sh
+++ secrets/decrypt_secret.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SECRET_PASSPHRASE" \
---output $HOME/secrets/gcp_secret.json ./secrets/encrypted_key_file_gcp.gpg
+--output ./secrets/gcp_secret.json ./secrets/encrypted_key_file_gcp.gpg
