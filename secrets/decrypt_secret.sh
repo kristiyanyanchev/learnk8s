3dd035fa47259a00bbf835f6acbc07ed76c3babc
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2024-03-09 19:27:53 +0200

    fix

--- secrets/decrypt_secret.sh
+++ secrets/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SECRET_PASSPHRASE" \
---output ./secrets/gcp_secret.json ./secrets/encrypted_key_file_gcp.gpg
+--output $HOME/secrets/gcp_secret.json ./secrets/encrypted_key_file_gcp.gpg
